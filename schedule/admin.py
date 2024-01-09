780faaeafc5298dfe60772843638d0d1679de74e
Colin <colinscott.4388@gmail.com>
2024-01-09 13:51:16 -0500

    modified:   db.sqlite3 	modified:   schedule/__pycache__/admin.cpython-312.pyc 	modified:   schedule/__pycache__/models.cpython-312.pyc 	modified:   schedule/admin.py 	new file:   schedule/migrations/0008_shiftmarketplace_alter_schedule_datestarting_and_more.py 	new file:   schedule/migrations/0009_remove_shiftmarketplace_user_and_more.py 	new file:   schedule/migrations/__pycache__/0008_shiftmarketplace_alter_schedule_datestarting_and_more.cpython-312.pyc 	new file:   schedule/migrations/__pycache__/0009_remove_shiftmarketplace_user_and_more.cpython-312.pyc 	modified:   schedule/models.py 	modified:   templates/scheduleuser.html

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Schedule
+from .models import Schedule, ShiftMarketplace
 
 admin.site.register(Schedule)
+admin.site.register(ShiftMarketplace)
--- schedule/models.py
+++ schedule/models.py
@@ -10,3 +10,12 @@ class Schedule(models.Model):
     jobCode = models.CharField(max_length=20)
     def __str__(self):
         return f"{self.user} ({(self.startTime).strftime("%-I:%M %p")} - {(self.endTime).strftime("%-I:%M %p")} / {self.dateStarting})"
+
+class ShiftMarketplace(models.Model):
+    dateStarting = models.DateField(default=datetime.datetime.now())
+    startTime = models.TimeField(default=datetime.datetime.now())
+    endTime = models.TimeField(default=datetime.datetime.now())
+    jobCode = models.CharField(max_length=20)
+    def __str__(self):
+        return f"({(self.startTime).strftime("%-I:%M %p")} - {(self.endTime).strftime("%-I:%M %p")} / {self.dateStarting})"
+    
